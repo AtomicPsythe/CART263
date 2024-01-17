bdb7def37ed0701b3c57377f034eda0eda31b529
AtomicPsythe <fotibuddy@gmail.com>
2024-01-17 12:34:30 -0500

    Completed Where's the Sausage Dog activity but with minor errors that need to be looked over

--- Activities/wheres-sausage-dog/js/script.js
+++ Activities/wheres-sausage-dog/js/script.js
@@ -1,33 +1,67 @@
 /**
-Title of Project
-Author Name
-
-This is a template. You must fill in the title,
-author, and this description to match your project!
+Activity 1 - Where's Sausage Dog
+Foti Aivaliklis
 */
 
 "use strict";
 
+const NUM_ANIMAL_IMAGES = 10;
+const NUM_ANIMALS = 100;
+
+let animalImages = [];
+let animals = [];
+
+let sausageDogImage = undefined;
+let sausageDog = undefined;
 
 /**
-Description of preload
+Creates a For loop for all of the animal + sausage dog images
 */
 function preload() {
+    for (let i = 0; i < NUM_ANIMAL_IMAGES, i++;) {
+        let animalImage = loadImage("assets/images/animal${i}.png");
+        animalImages.push(animalImage);
+    }
 
+    sausageDogImage = loadImage("assets/images/sausage-dog.png");
 }
 
 
 /**
-Description of setup
+Creates a For loop for the animal + sausage dog images and sets them all at a random location
 */
 function setup() {
+    createCanvas(windowWidth, windowHeight);
 
+    // create the animals
+    for (let i = 0, i < NUM_ANIMALS, i++;) {
+        let x = random(0, width);
+        let y = random(0, height);
+        let animalImage = random(animalImages);
+        let animal = new Animal(x, y, animalImage);
+        animals.push(animal);
+    }
+
+    let x = random(0, width);
+    let y = random(0, height);
+    sausageDog = new sausageDog(x, y, sausageDogImage);
 }
 
 
 /**
-Description of draw()
+Creates the program itself (displays the images and the background)
 */
 function draw() {
+    background(255, 255, 0);
 
+    for (let i = 0, i < animals.length; i++) {
+        animals[i].update();
+    }
+
+    sausageDog.update();
+}
+
+// confirms that the sausage dog was pressed when the mouse clicks on it
+function mousePressed() {
+    sausageDog.mousePressed();
 }
