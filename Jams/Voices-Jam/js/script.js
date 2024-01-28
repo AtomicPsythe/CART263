0b89ced7e42454210540900fdabe6be2c4b8cfac
AtomicPsythe <fotibuddy@gmail.com>
2024-01-28 15:51:42 -0500

    all of the questions + answers are now being read by the computer just need to figure out how to make the computer understand what the user says

--- Jams/Voices-Jam/js/script.js
+++ Jams/Voices-Jam/js/script.js
@@ -152,27 +152,165 @@ function q2() {
     pop();   
 }
 
+function q3() {
+    push();
+    textSize(48);
+    fill(159, 51, 51);
+    textAlign(CENTER, CENTER);
+    text("What is the best selling video \n game of all time?", width/2, height/3);
+    pop();
+    push();
+    textSize(26);
+    fill(159, 51, 51);
+    textAlign(CENTER);
+    text("A) Tetris", width/2, height/1.8)
+    pop();
+    push();
+    textSize(26);
+    fill(159, 51, 51);
+    textAlign(CENTER);
+    text("B) Minecraft", width/2, height/1.6)
+    pop();
+    push();
+    textSize(26);
+    fill(159, 51, 51);
+    textAlign(CENTER);
+    text("C) Grand Theft Auto", width/2, height/1.4)
+    pop();   
+}
+
+function q4() {
+    push();
+    textSize(48);
+    fill(159, 51, 51);
+    textAlign(CENTER, CENTER);
+    text("What is the best selling video \n game console of all time?", width/2, height/3);
+    pop();
+    push();
+    textSize(26);
+    fill(159, 51, 51);
+    textAlign(CENTER);
+    text("A) Nintendo DS", width/2, height/1.8)
+    pop();
+    push();
+    textSize(26);
+    fill(159, 51, 51);
+    textAlign(CENTER);
+    text("B) Sony Playstation 2", width/2, height/1.6)
+    pop();
+    push();
+    textSize(26);
+    fill(159, 51, 51);
+    textAlign(CENTER);
+    text("C) XBox 360", width/2, height/1.4)
+    pop();   
+}
+
+function q5() {
+    push();
+    textSize(48);
+    fill(159, 51, 51);
+    textAlign(CENTER, CENTER);
+    text("What is the species of Sonic \n the Hedgehog's friend Knuckles?", width/2, height/3);
+    pop();
+    push();
+    textSize(26);
+    fill(159, 51, 51);
+    textAlign(CENTER);
+    text("A) Two-tailed fox", width/2, height/1.8)
+    pop();
+    push();
+    textSize(26);
+    fill(159, 51, 51);
+    textAlign(CENTER);
+    text("B) Robot", width/2, height/1.6)
+    pop();
+    push();
+    textSize(26);
+    fill(159, 51, 51);
+    textAlign(CENTER);
+    text("C) Echidna", width/2, height/1.4)
+    pop();   
+}
+
 function handleSpeechInput() {
     console.log(speechRecognizer.resultString);
     // question 1
     if (speechRecognizer.resultString === "Donkey Kong") {
         correct.play();
+        state = "q2"
+        speechSynthesizer.speak("Which of these games is oldest? A) Asteroids, B) Galaxian, C) Pong")
     }
     if (speechRecognizer.resultString === "Super Mario Bros") {
         incorrect.play();
+        state = "q2"
+        speechSynthesizer.speak("Which of these games is oldest? A) Asteroids, B) Galaxian, C) Pong")
     }
     if (speechRecognizer.resultString === "Mario's Cement Factory") {
         incorrect.play();
+        state = "q2"
+        speechSynthesizer.speak("Which of these games is oldest? A) Asteroids, B) Galaxian, C) Pong")
     }
     // question 2
     if (speechRecognizer.resultString === "Asteroids") {
-        correct.play();
+        incorrect.play();
+        state = "q3"
+        speechSynthesizer.speak("What is the best selling video game of all time? A) Tetris, B) Minecraft, C) Grand Theft Auto")
     }
     if (speechRecognizer.resultString === "Galaxian") {
         incorrect.play();
+        state = "q3"
+        speechSynthesizer.speak("What is the best selling video game of all time? A) Tetris, B) Minecraft, C) Grand Theft Auto")
     }
     if (speechRecognizer.resultString === "Pong") {
+        correct.play();
+        state = "q3"
+        speechSynthesizer.speak("What is the best selling video game of all time? A) Tetris, B) Minecraft, C) Grand Theft Auto")
+    }
+    // question 3
+    if (speechRecognizer.resultString === "Tetris") {
+        correct.play();
+        state = "q4"
+        speechSynthesizer.speak("What is the best selling video game console of all time? A) Nintendo DS, B) Sony Playstation 2, C) XBox 360")
+    }
+    if (speechRecognizer.resultString === "Minecraft") {
         incorrect.play();
+        state = "q4"
+        speechSynthesizer.speak("What is the best selling video game console of all time? A) Nintendo DS, B) Sony Playstation 2, C) XBox 360")
+    }
+    if (speechRecognizer.resultString === "Grand Theft Auto") {
+        incorrect.play();
+        state = "q4"
+        speechSynthesizer.speak("What is the best selling video game console of all time? A) Nintendo DS, B) Sony Playstation 2, C) XBox 360")
+    }
+    // question 4
+    if (speechRecognizer.resultString === "Nintendo DS") {
+        incorrect.play();
+        state = "q5"
+        speechSynthesizer.speak("What is the species of Sonic the Hedgehog's friend Knuckles? A) Two-tailed fox, B) Robot, C) Echidna")
+    }
+    if (speechRecognizer.resultString === "Sony Playstation 2") {
+        correct.play();
+        state = "q5"
+        speechSynthesizer.speak("What is the species of Sonic the Hedgehog's friend Knuckles? A) Two-tailed fox, B) Robot, C) Echidna")
+    }
+    if (speechRecognizer.resultString === "XBox 360") {
+        incorrect.play();
+        state = "q5"
+        speechSynthesizer.speak("What is the species of Sonic the Hedgehog's friend Knuckles? A) Two-tailed fox, B) Robot, C) Echidna")
+    }
+    // question 5
+    if (speechRecognizer.resultString === "Two-tailed fox") {
+        incorrect.play();
+        state = "ending"
+    }
+    if (speechRecognizer.resultString === "Robot") {
+        incorrect.play();
+        state = "ending"
+    }
+    if (speechRecognizer.resultString === "Echidna") {
+        correct.play();
+        state = "ending"
     }
 }
 
@@ -183,10 +321,16 @@ function mousePressed() {
     if (state === "q1") {
         speechSynthesizer.speak("What video game did Mario first appear in? A) Donkey Kong, B) Super Mario Bros., C) Mario's Cement Factory");
     }
-    if (state === "q1") {
-        state === "q2";
-    }
     if (state === "q2") {
         speechSynthesizer.speak("Which of these games is oldest? A) Asteroids, B) Galaxian, C) Pong")
+    }
+    if (state === "q3") {
+        speechSynthesizer.speak("What is the best selling video game of all time? A) Tetris, B) Minecraft, C) Grand Theft Auto")
+    }
+    if (state === "q4") {
+        speechSynthesizer.speak("What is the best selling video game console of all time? A) Nintendo DS, B) Sony Playstation 2, C) XBox 360")
+    }
+    if (state === "q5") {
+        speechSynthesizer.speak("What is the species of Sonic the Hedgehog's friend Knuckles? A) Two-tailed fox, B) Robot, C) Echidna")
     }
 }
