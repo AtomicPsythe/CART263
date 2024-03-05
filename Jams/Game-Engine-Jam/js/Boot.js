52efc4a55a0d6b56d66da72434ecdf0544bc0ea4
AtomicPsythe <fotibuddy@gmail.com>
2024-03-04 21:54:22 -0500

    started the game engine jam

---
+++ Jams/Game-Engine-Jam/js/Boot.js
@@ -0,0 +1,26 @@
+class Boot extends Phaser.Scene {
+
+    constructor() {
+      super({
+        key: `boot`
+      });
+    }
+  
+    create() {
+      let loadingTextStyle = {
+        fontFamily: "sans-serif",
+        fontSize: "40px",
+        fill: "#ffffff",
+        align: "center"
+      };
+      let loadingString = `Loading...`;
+      this.loadingText = this.add.text(100, 100, loadingString, loadingTextStyle);
+  
+      // Switch to the scene with the key of "play"
+      this.scene.start(`play`);
+    }
+  
+    update() {
+  
+    }
+  }
