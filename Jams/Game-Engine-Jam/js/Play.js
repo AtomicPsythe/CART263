bda61425bc40fc5011d64259ad969bd4798106ad
AtomicPsythe <fotibuddy@gmail.com>
2024-03-06 18:51:35 -0500

    made the maze properly appear, however now the character is gone

--- Jams/Game-Engine-Jam/js/Play.js
+++ Jams/Game-Engine-Jam/js/Play.js
@@ -7,17 +7,55 @@ class Play extends Phaser.Scene {
 
     create() {
         console.log("Play scene created!");
+        this.maze = this.physics.add.sprite(400, 300, "maze");
+        this.avatar = this.physics.add.sprite(150, 590, "avatar");
+        console.log(this.avatar);
+        
+        this.createAnimations();
 
-        this.add.image(400, 300, 'maze');
-        // this.maze = this.physics.add.group({
-        //     key: "maze",
-        //     immovable: true,
-        //     quantity: 1
-        // })
+        this.avatar.setVelocityX(100);
 
-        this.avatar = this.add.sprite(50, 590, "avatar");
+        this.avatar.play("idle");
+
+        this.cursors = this.input.keyboard.createCursorKeys();
+    }
+
+    update() {
+        this.handleInput();
+    }
+
+    handleInput() {
+        if (this.cursors.left.isDown) {
+            this.avatar.setVelocityX(-100);
+        }
+        else if (this.cursors.right.isDown) {
+            this.avatar.setVelocityX(100);
+        }
+        else {
+            this.avatar.setVelocityX(0);
+        }
+
+        if (this.cursors.up.isDown) {
+            this.avatar.setVelocityY(-100);
+        }
+        else if (this.cursors.down.isDown) {
+            this.avatar.setVelocityY(100);
+        }
+        else {
+            this.avatar.setVelocityY(0);
+        }
+
+        if (this.avatar.body.velocity.x !== 0 || this.avatar.body.velocity.y !== 0) {
+            this.avatar.play(`moving`, true);
+        }
+        else {
+            this.avatar.play(`idle`, true);
+        }
+    }
+
+    createAnimations() {
         this.anims.create({
-            key: "avatar-moving",
+            key: "moving",
             frames: this.anims.generateFrameNumbers("avatar", {
                 start: 0,
                 end: 7
@@ -26,17 +64,14 @@ class Play extends Phaser.Scene {
             repeat: -1
         });
 
-        // let movingAnimationConfig = {
-        //     key: "moving",
-        //     frames: this.anims.generateFrameNumbers("avatar", {
-        //         start: 0,
-        //         end: 31
-        //     }),
-        //     frameRate: 24, 
-        //     repeat: -1
-        // }
-        // this.anims.create(movingAnimationConfig);
-
-        this.avatar.play("avatar-moving");
+        let idleAnimationConfig = {
+            key: "idle",
+            frames: this.anims.generateFrameNumbers("avatar", {
+                start: 0,
+                end: 0
+            }),
+            repeat: 0
+        };
+        this.anims.create(idleAnimationConfig);
     }
 }
