4e59144b6d79da11be298336ad82977259736f7f
AtomicPsythe <fotibuddy@gmail.com>
2024-03-31 13:12:59 -0400

    Update Title.js

--- Final Project/Lost-In-The-Abyss/js/Title.js
+++ Final Project/Lost-In-The-Abyss/js/Title.js
@@ -29,6 +29,6 @@ class Title extends Phaser.Scene {
     
         // once the fade is completed, it will transition to the play scene
         this.cameras.main.once(Phaser.Cameras.Scene2D.Events.FADE_OUT_COMPLETE, (cam, effect) => {
-            this.scene.start('maze2')
+            this.scene.start('introText')
     })}
 }
